3a8f7808c3b41e6a5006071b1f7055ba6fbca724
Hannah Zhu <t-hannzhu@microsoft.com>
2022-06-28 16:37:42 +0000

    added a screenshot to see state of test

--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -5,6 +5,8 @@ test('login from home page', async ({ page }) => {
   await page.waitForSelector('text=Login to see todo items');
   await page.click('a:has-text("Login")');
   await expect(page).toHaveURL('/.auth/login/github');
+  await page.waitForSelector('button');
+  await page.screenshot({ path: 'my_screenshot.png', fullPage: true });
   await page.click('text=Login');
   await page.waitForSelector('span');
   await expect(page.locator('span')).toContainText('Welcome');
