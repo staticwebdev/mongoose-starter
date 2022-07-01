316fb067debf25d835857490895ba9ea0f495782
Hannah Zhu <t-hannzhu@microsoft.com>
2022-07-01 18:22:42 +0000

    making sure this workflow run is the same as the local one

--- playwright.config.ts
+++ playwright.config.ts
@@ -94,14 +94,15 @@ const config: PlaywrightTestConfig = {
     // },
   ],
 
+  
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 4280,
-  // }
+  webServer: {
+    command: 'npm run start',
+    port: 4280,
+  }
 };
 
 export default config;
