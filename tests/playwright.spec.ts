0884a42978ed35c2da154f0ead4b19013d368946
Hannah Zhu <t-hannzhu@microsoft.com>
2022-07-22 16:22:39 +0000

    added new basic test and modified login test

--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -1,13 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('login from home page', async ({ page }) => {
+test('basic manager', async ({ page }) => {
+  await page.goto('/');
+  await page.waitForSelector('strong');
+  await expect(page.locator('strong')).toContainText('Todo manager')
+}) 
+
+test('login button', async ({ page }) => {
   await page.goto('/');
   await page.waitForSelector('text=Login to see todo items');
   await page.click('a:has-text("Login")');
   await expect(page).toHaveURL('/.auth/login/github');
-  await page.waitForSelector('button');
-  await page.screenshot({ path: 'my_screenshot.png', fullPage: true });
-  await page.click('text=Login');
-  await page.waitForSelector('span');
-  await expect(page.locator('span')).toContainText('Welcome');
-})
+}) 
